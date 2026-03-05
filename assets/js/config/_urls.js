e44488d9a3d9bcc29a4529beb441c75e460d02cd
elnursr <elnursultanv7@gmail.com>
2026-03-05 08:35:00 +0400

    added all brand icons content

--- assets/js/config/_urls.js
+++ assets/js/config/_urls.js
@@ -1,2 +1,3 @@
 export let font_awesome_v_4_7_url = 'assets/json/_font_awesome_v_4.7.json',
-    font_awesome_v_5_15_4_url = 'assets/json/_font_awesome_v_5.15.4.json';
+    font_awesome_v_5_15_4_url = 'assets/json/_font_awesome_v_5.15.4.json',
+    font_awesome_v_6_7_2_url = 'assets/json/_font_awesome_v_6.7.2.json';
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ import { BrandIcons } from './brand_icons/_brand_icons.js';
 
 import { brandIconsElement, brandIconTitleElement } from './ui/_dom_selectors.js';
 
-import { font_awesome_v_4_7_url, font_awesome_v_5_15_4_url } from './config/_urls.js';
+import { font_awesome_v_4_7_url, font_awesome_v_5_15_4_url, font_awesome_v_6_7_2_url } from './config/_urls.js';
 
 
 // let v_4_7 = new BrandIcons({
@@ -22,13 +22,22 @@ import { font_awesome_v_4_7_url, font_awesome_v_5_15_4_url } from './config/_url
 let v_5_15_4 = new BrandIcons({
     isShadow: true,
     iconVersion: 'v_5_15_4',
-    iconEffect: ['highlight'],
+    iconEffect: ['flash'],
     // iconEffectTitle: ['Highlight', 'Flash'],
     // iconEffect: ['scale', 'fill', 'flash', 'diamond', 'blocks-in', 'circle-scale', 'circle-rotate', 'rotate-scale-clockwise', 'rotate-top-left-clockwise', 'highlight'],
     iconVersionUrl: font_awesome_v_5_15_4_url
 });
 
-v_5_15_4.toUI({
+let v_6_7_2 = new BrandIcons({
+    isShadow: true,
+    iconVersion: 'v_6_7_2',
+    iconEffect: ['flash'],
+    // iconEffectTitle: ['Highlight', 'Flash'],
+    // iconEffect: ['scale', 'fill', 'flash', 'diamond', 'blocks-in', 'circle-scale', 'circle-rotate', 'rotate-scale-clockwise', 'rotate-top-left-clockwise', 'highlight'],
+    iconVersionUrl: font_awesome_v_6_7_2_url
+});
+
+v_6_7_2.toUI({
     iconsElement: brandIconsElement,
     titleElement: brandIconTitleElement
 });
